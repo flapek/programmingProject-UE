f67379ab26c588550a2f9ab37d987f90822b1fb9
Marcin Filapek <filap4@gmail.com>
2022-03-24 21:13:30 +0100

    Revert "update config" and add pagination
    This reverts commit 942767eb79ba538adf061ca862bdae43e8b867b7.

--- front/src/Pages/Home.tsx
+++ front/src/Pages/Home.tsx
@@ -1,5 +1,73 @@
-import React from 'react';
+import { Box, styled, Paper, TablePagination } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  textAlign: 'center',
+  height: 60,
+  lineHeight: '60px',
+  width: '100%',
+}));
 
 export default function Home() {
-  return <div>Home</div>;
+  const [elements, setElements] = useState<number[]>([]);
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  useEffect(() => {
+    setElements([...Array(20).keys()]);
+  }, []);
+
+  const handleChangePage = (
+    event: React.MouseEvent<HTMLButtonElement> | null,
+    newPage: number,
+  ) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  return (
+    <React.Fragment>
+      <TablePagination
+        component="div"
+        count={elements.length}
+        page={page}
+        onPageChange={handleChangePage}
+        rowsPerPage={rowsPerPage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+      <Box
+        sx={{
+          p: 2,
+          bgcolor: 'background.default',
+          display: 'flex',
+          flexDirection: 'column',
+          gridTemplateColumns: { md: '1fr 1fr' },
+          gap: 2,
+        }}
+      >
+        {elements
+          .slice(page * rowsPerPage, (page + 1) * rowsPerPage)
+          .map((item, idx) => (
+            <Item key={idx} elevation={4}>
+              {`idx=${item}`}
+            </Item>
+          ))}
+      </Box>
+      <TablePagination
+        component="div"
+        count={elements.length}
+        page={page}
+        onPageChange={handleChangePage}
+        rowsPerPage={rowsPerPage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </React.Fragment>
+  );
 }
