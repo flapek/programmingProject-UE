ad9705c0144db117e663b125f31840f3170d1812
Mateusz K <mateusz.kos1@edu.uekat.pl>
2022-04-21 20:03:00 +0200

    update scraper main

--- services/scrapper/main.py
+++ services/scrapper/main.py
@@ -1,5 +1,3 @@
 from add_product import add_product
-from get_info import get_info_one_product
 
-#print(add_product('https://www.x-kom.pl/p/715689-karta-graficzna-nvidia-gigabyte-geforce-rtx-3050-gaming-oc-8gb-gddr6.html'))
-print(get_info_one_product('https://www.x-kom.pl/p/715689-karta-graficzna-nvidia-gigabyte-geforce-rtx-3050-gaming-oc-8gb-gddr6.html'))
+print(add_product('https://www.x-kom.pl/p/715689-karta-graficzna-nvidia-gigabyte-geforce-rtx-3050-gaming-oc-8gb-gddr6.html'))
