68fda259f276cdb07232afbe2e2d684723a245fe
Mateusz K <mateusz.kos1@edu.uekat.pl>
2022-04-25 15:18:34 +0200

    update scraper

--- services/main.py
+++ services/main.py
@@ -10,9 +10,3 @@ def crawler_send(base_url, category_url):
         send(link[0])
 
 crawler_send(base_url, category_url)
-#add_product('https://www.x-kom.pl/p/715689-karta-graficzna-nvidia-gigabyte-geforce-rtx-3050-gaming-oc-8gb-gddr6.html')
-
-#send('https://www.x-kom.pl/p/715689-karta-graficzna-nvidia-gigabyte-geforce-rtx-3050-gaming-oc-8gb-gddr6.html')
-
-#print(get_info_one_product('https://www.x-kom.pl/p/717636-karta-graficzna-nvidia-palit-geforce-rtx-3050-dual-8gb-gddr6.html'))
-#print(get_info_onsse_product('https://www.x-kom.pl/p/715689-karta-graficzna-nvidia-gigabyte-geforce-rtx-3050-gaming-oc-8gb-gddr6.html'))
