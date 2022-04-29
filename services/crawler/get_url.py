67a570a87c7297fc3dc70ea6f32d68bdd3a9c977
Mateusz K <mateusz.kos1@edu.uekat.pl>
2022-04-29 16:59:06 +0200

    update crawler and test_calc

--- services/crawler/get_url.py
+++ services/crawler/get_url.py
@@ -14,13 +14,15 @@ def get_prod_url(base_url: str, url: str):
     while url:
         crawl_page = base_url + url
         html_page = urlopen(Request(crawl_page))
-        # print(crawl_page)
+        #print(crawl_page)
         soup = BeautifulSoup(html_page, "lxml")
         for link in soup.findAll('a', class_='sc-1h16fat-0 sc-1yu46qn-7 kaqYqE'):
-            links.append('https://www.x-kom.pl'+link.get('href'))
-        # print(links)
-        # print(len(links))
+            links.append({
+                'href': 'https://www.x-kom.pl'+link.get('href')
+            })
+        #print(links)
+        #print(len(links))
         next_btn = soup.find(class_='sc-1xy3kzh-8 jpsIZh sc-1h16fat-0 irSQpN')
         url = next_btn.get('href') if next_btn else None
-        sleep(randint(1, 3))
+        sleep(1)
     return links
