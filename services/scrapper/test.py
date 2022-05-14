12a56edc1abade6259412f9c949e90cdc48b2cf7
Mateusz K <mateusz.kos1@edu.uekat.pl>
2022-05-14 15:46:30 +0200

    update test.py

--- services/scrapper/test.py
+++ services/scrapper/test.py
@@ -1,22 +1,34 @@
 import unittest
-import get_info
 import services.crawler.get_url
+from bs4 import BeautifulSoup
+import cloudscraper
+import configparser
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+url = config['url']['base_url'] + config['url']['category_url']
+scraper = cloudscraper.create_scraper()
+response = scraper.get(url)
+open('testing_site.html', 'wb').write(response.content)
 
 
 class Test(unittest.TestCase):
-    def test_link(self):
-        result = services.crawler.get_url.get_prod_url('https://www.x-kom.pl', '/g-5/c/345-karty-graficzne.html?page=1')[0]['href']
-        self.assertEqual(result, 'https://www.x-kom.pl/p/480513-karta-graficzna-nvidia-gigabyte-geforce-gtx-1660-ti-oc-6gb-gddr6.html')
-
-    def test_price(self):
-        result = get_info.get_info_one_product('https://www.x-kom.pl/p/480513-karta-graficzna-nvidia-gigabyte-geforce-gtx-1660-ti-oc-6gb-gddr6.html')['price']
-        self.assertEqual(result, 1599.0)
+    def setUp(self) -> None:
+        self.result = services.crawler.get_url.get_prod_url('https://www.x-kom.pl',
+                                                            '/g-5/c/345-karty-graficzne.html?page=1')[0]['href']
 
     def test_method(self):
-        result = get_info.get_info_one_product('https://www.x-kom.pl/p/480513-karta-graficzna-nvidia-gigabyte-geforce-gtx-1660-ti-oc-6gb-gddr6.html')
-        self.assertEqual(result, {'name': 'Gigabyte GeForce GTX 1660 Ti OC 6GB GDDR6',
-                                  'gpu': 'GeForce GTX 1660 Ti', 'price': 1599.,
-                                  'image': 'https://cdn.x-kom.pl/i/setup/images/prod/big/product-new-big,,2019/2/pr_2019_2_18_14_33_9_864_05.jpg'})
+        with open('testing_site.html', "r", encoding='utf-8') as f:
+            text = f.read()
+        soup = BeautifulSoup(text, "lxml")
+        links = []
+        for link in soup.findAll('a', class_='sc-1h16fat-0 sc-1yu46qn-7 kaqYqE'):
+            links.append({
+                'href': 'https://www.x-kom.pl' + link.get('href')
+            })
+        self.assertEqual(links[0]['href'], self.result)
+
 
 if __name__ == '__main__':
     unittest.main()
