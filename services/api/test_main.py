d9e244e8374f078e7eddac329e535db395f9bbda
Weronika Ładak <weronika.ladak1@gmail.com>
2022-05-19 11:43:26 +0200

    renamed tests

--- services/api/test_main.py
+++ services/api/test_main.py
@@ -18,17 +18,19 @@ names = [
 ]
 
 
-def test_get_items_return_json():
+def test_get_items_correct_status_code():
     response = client.get("/")
     assert response.status_code == 200
+
+def test_get_items_return_json():
+    response = client.get("/")
     assert type(response.json()) == str
     assert re.search(r"\[.*]", response.json()) is not None
 
-
-def test_search():
+def test_search_return_items_with_a():
     db.drop()
-    for element in names:
-        db.add({"name": element})
+    for item in names:
+        db.add({"name": item})
 
     assert db.search("a") == [
         "Adam",
@@ -39,32 +41,119 @@ def test_search():
         "adam",
         "family",
     ]
+
+def test_search_return_one_item_in_list():
+    db.drop()
+    for item in names:
+        db.add({"name": item})
     assert db.search("fl") == ["fLOwer"]
+
+def test_search_return_empty_list():
+    db.drop()
+    for item in names:
+        db.add({"name": item})
     assert db.search("z") == []
+
+def test_search_return_entire_list():
+    db.drop()
+    for item in names:
+        db.add({"name": item})
     assert db.search("") == names
 
 
-def test_find():
+def test_find_return_length_of_list():
+    db.drop()
+    size = len(names)
+    for item in names:
+        db.add({"name": item})
+    assert len(json.loads(db.find(size, 0, None))) == size
+
+def test_find_return_length_of_list_with_skip():
     db.drop()
     skip = 2
     size = len(names)
-    for element in names:
-        db.add({"name": element})
-    assert len(json.loads(db.find(size, 0, None))) == size
+    for item in names:
+        db.add({"name": item})
     assert len(json.loads(db.find(size, skip, None))) == size - skip
+
+def test_find_more_than_length_of_list_return_length_of_list_with_skip():
+    db.drop()
+    skip = 2
+    size = len(names)
+    for item in names:
+        db.add({"name": item})
     assert len(json.loads(db.find(size + 100, skip, None))) == size - skip
+
+def test_find_return_zero():
+    db.drop()
+    size = len(names)
+    for item in names:
+        db.add({"name": item})
     assert len(json.loads(db.find(size, size + 1, None))) == 0
+
+def test_find_return_three():
+    db.drop()
+    size = len(names)
+    for item in names:
+        db.add({"name": item})
     assert len(json.loads(db.find(size, 0, "adam"))) == 3
+
+def test_find_return_one():
+    db.drop()
+    skip = 2
+    size = len(names)
+    for item in names:
+        db.add({"name": item})
     assert len(json.loads(db.find(size, skip, "adam"))) == 1
+
+def test_find_return_entire_list():
+    db.drop()
+    size = len(names)
+    for item in names:
+        db.add({"name": item})
     assert [x["name"] for x in json.loads(db.find(size, 0, None))] == names
+
+def test_find_return_list_with_skip():
+    db.drop()
+    skip = 2
+    size = len(names)
+    for item in names:
+        db.add({"name": item})
     assert [x["name"] for x in json.loads(db.find(size, skip, None))] == names[
         skip:
     ]
+
+def test_find_return_list_with_three_itmes():
+    db.drop()
+    size = len(names)
+    for item in names:
+        db.add({"name": item})
     assert [x["name"] for x in json.loads(db.find(size, 0, "adAM"))] == [
         "Adam",
         "adaM",
         "adam",
     ]
+
+def test_find_return_list_with_one_item_because_of_skip():
+    db.drop()
+    skip = 2
+    size = len(names)
+    for item in names:
+        db.add({"name": item})
     assert [x["name"] for x in json.loads(db.find(size, skip, "adAM"))] == [
         "adam"
     ]
+
+def test_find_return_empty_list():
+    db.drop()
+    size = len(names)
+    for item in names:
+        db.add({"name": item})
+    assert [x["name"] for x in json.loads(db.find(size, 0, "z"))] == []
+    
+def test_find_return_list_with_two_items():
+    db.drop()
+    size = len(names)
+    for item in names:
+        db.add({"name": item})
+    assert [x["name"] for x in json.loads(db.find(size, 0, "f"))] == ["fLOwer", "family"]
