f9837a2a5e3604b5315fcb3661c3a6b2bd7d7d0d
Mateusz K <mateusz.kos1@edu.uekat.pl>
2022-05-19 13:17:13 +0200

    update crawler

--- services/crawler/add_url.py
+++ services/crawler/add_url.py
@@ -3,10 +3,6 @@ import pymongo
 from datetime import datetime
 
 
-# base_url = 'https://www.x-kom.pl'
-# category_url = '/g-5/c/345-karty-graficzne.html?page=1'
-
-
 def add_product(base_url: str, url: str):
     links = get_prod_url(base_url,url)
     now = datetime.utcnow().replace(microsecond=0).isoformat("T")
@@ -18,8 +14,6 @@ def add_product(base_url: str, url: str):
                 "url": link
             }]
         }]
-        # print(link)
-        # print(json_body)
         myclient = pymongo.MongoClient("mongodb://localhost:27017/", username='root', password='example')
         mydb = myclient["mydatabase"]
         mycol = mydb["links"]
--- services/crawler/get_url.py
+++ services/crawler/get_url.py
@@ -1,12 +1,6 @@
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
 from time import sleep
-import json
-from random import randint
-
-
-# base_url = 'https://www.x-kom.pl'
-# category_url = '/g-5/c/345-karty-graficzne.html?page=1'
 
 
 def get_prod_url(base_url: str, url: str):
@@ -14,14 +8,11 @@ def get_prod_url(base_url: str, url: str):
     while url:
         crawl_page = base_url + url
         html_page = urlopen(Request(crawl_page))
-        #print(crawl_page)
         soup = BeautifulSoup(html_page, "lxml")
         for link in soup.findAll('a', class_='sc-1h16fat-0 sc-1yu46qn-7 kaqYqE'):
             links.append({
                 'href': 'https://www.x-kom.pl'+link.get('href')
             })
-        #print(links)
-        #print(len(links))
         next_btn = soup.find(class_='sc-1xy3kzh-8 jpsIZh sc-1h16fat-0 irSQpN')
         url = next_btn.get('href') if next_btn else None
         sleep(1)
