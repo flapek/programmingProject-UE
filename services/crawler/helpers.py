f3d15474f9f1adac1ca9c665629d34d939843441
flapek <46303285+flapek@users.noreply.github.com>
2022-05-28 21:33:44 +0200

    Update helpers.py

--- services/crawler/helpers.py
+++ services/crawler/helpers.py
@@ -7,6 +7,7 @@ from time import sleep
 HREF = "href"
 LINK_CSS_CLASS = "sc-1h16fat-0 sc-1yu46qn-7 kaqYqE"
 SOUP_CSS_CLASS = "sc-1xy3kzh-8 jpsIZh sc-1h16fat-0 irSQpN"
+PARSER_TYPE = "lxml"
 
 
 def get_prod_url(base_url: str, url: str):
@@ -14,7 +15,7 @@ def get_prod_url(base_url: str, url: str):
     while url:
         crawl_page = base_url + url
         html_page = urlopen(Request(crawl_page))
-        soup = BeautifulSoup(html_page, "lxml")
+        soup = BeautifulSoup(html_page, PARSER_TYPE)
         for link in soup.findAll("a", class_=LINK_CSS_CLASS):
             links.append({HREF: base_url + link.get(HREF)})
         next_btn = soup.find(class_=SOUP_CSS_CLASS)
