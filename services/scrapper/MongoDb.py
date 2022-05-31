5cd1c372cb9b58e06946cd05b8ed7613ff74aa00
flapek <46303285+flapek@users.noreply.github.com>
2022-05-31 21:57:01 +0200

    update database model

--- services/scrapper/MongoDb.py
+++ services/scrapper/MongoDb.py
@@ -22,25 +22,35 @@ class MongoDb:
                     "value": product["price"],
                 }
             ],
-            "ray_tracing": product["ray_tracing"],
-            "type_of_connector": product["type_of_connector"],
-            "memory": product["memory"],
-            "type_of_memory": product["type_of_memory"],
-            "effective_memory_timing": product["effective_memory_timing"],
-            "core_clock": product["core_clock"],
-            "CUDA_cores": product["CUDA_cores"],
-            "cooling_type": product["cooling_type"],
-            "types_of_outputs": product["types_of_outputs"],
-            "supported_libraries": product["supported_libraries"],
-            "power_supply_connector": product["power_supply_connector"],
-            "recommended_power_supply": product["recommended_power_supply"],
-            "power_consumption": product["power_consumption"],
-            "length": product["length"],
-            "width": product["width"],
-            "height": product["height"],
-            "warranty": product["warranty"],
-            "manufacturer_code": product["manufacturer_code"],
-            "x-kom_code": product["x-kom_code"],
+            "specyfication": [
+                {"ray_tracing": product["ray_tracing"]},
+                {"type_of_connector": product["type_of_connector"]},
+                {"memory": product["memory"]},
+                {"type_of_memory": product["type_of_memory"]},
+                {
+                    "effective_memory_timing": product[
+                        "effective_memory_timing"
+                    ]
+                },
+                {"core_clock": product["core_clock"]},
+                {"CUDA_cores": product["CUDA_cores"]},
+                {"cooling_type": product["cooling_type"]},
+                {"types_of_outputs": product["types_of_outputs"]},
+                {"supported_libraries": product["supported_libraries"]},
+                {"power_supply_connector": product["power_supply_connector"]},
+                {
+                    "recommended_power_supply": product[
+                        "recommended_power_supply"
+                    ]
+                },
+                {"power_consumption": product["power_consumption"]},
+                {"length": product["length"]},
+                {"width": product["width"]},
+                {"height": product["height"]},
+                {"warranty": product["warranty"]},
+                {"manufacturer_code": product["manufacturer_code"]},
+                {"x-kom_code": product["x-kom_code"]},
+            ],
         }
 
         if self.__col.find_one({"name": product["name"]}) is not None:
